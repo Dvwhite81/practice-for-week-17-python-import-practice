ce23fd55297000491457d677e51edde213c00946
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-06 16:01:01 -0400

    Complete

--- python/office/idle.py
+++ python/office/idle.py
@@ -1,2 +1,18 @@
+from random import random
+from math import floor
 
-# Your code here
+def chat():
+    coworkers = ["Jack", "Lenny", "Michelle", "Andrea"]
+    chatee = coworkers[floor(random()*4)]
+    print("Chatting with " + chatee + "...")
+    print("Done")
+
+def getWater():
+    print("Getting water...")
+    print("That was refreshing.")
+
+def useSocialMedia():
+    socialMedia = ["FaceBook", "Twitter", "YouTube", "Reddit"]
+    choice = socialMedia[floor(random()*4)]
+    print("Using " + choice + "...")
+    print("Done")
